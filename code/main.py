2eae17ca7b19562d1d0fee87bfbac4ea5e44f4fc
aalto1 <aalto@null.net>
2017-11-26 11:17:20 -0500

    DomainAdaptationTask

--- code/main.py
+++ code/main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 #    obejct_recognition_task.train()
 #    obejct_recognition_task.test()
     
-    domains = {0: "amazon", 1: "dslr", 2: "webcam"}
+    #domains = {0: "amazon", 1: "dslr", 2: "webcam"}
     domain_adaptation_task = DomainAdaptationTask(origin_domain = "amazon", target_domain = "webcam", combo = "ST")
     domain_adaptation_task.train()
     domain_adaptation_task.test()
